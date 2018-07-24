43103824f97b5ae347541a20a48d2a59c852135d
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-24 16:09:30 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,25 +2,13 @@ def alphabetize(arr)
   spanish = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   spanish_array = spanish.chars
 
- 
-arr.each do |word|
-  puts word
-  x  = word.chars.collect do |letter|
-    spanish_array.find_index(letter)
+
+ sorted_array =  arr.sort_by do |word|
+   word.chars.collect do |letter|
+     spanish_array.find_index(letter)
+   end
+  end
+
+
+sorted_array
 end
-print x
-end  
-
-
-# sorted_array =  arr.sort_by do |word|
-#   word.chars.collect do |letter|
-#     spanish_array.find_index(letter)
-#   end
-#  end
-
-
-#sorted_array
-end
-
-expressions = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
-alphabetize(expressions)
